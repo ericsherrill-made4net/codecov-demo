46890607551326953368a51f8ec5930234c2175d
ericsherrill-made4net <187445060+ericsherrill-made4net@users.noreply.github.com>
2026-05-22 15:19:04 -0500

    step6: add Carryforward Flags

--- web/static/js/calculator.test.js
+++ web/static/js/calculator.test.js
@@ -1,6 +1,24 @@
 const calc = require('./calculator');
 
 describe('Calculator test suite', () => {
+  test('test operation', () => {
+    const calculator = new calc.Calculator();
+    calculator.chooseOperation('+');
+    expect(calculator.operation).toBeUndefined();
+
+    calculator.appendNumber(1);
+    calculator.appendNumber(2);
+    calculator.chooseOperation('+');
+    expect(calculator.currentOperand).toBe('');
+    expect(calculator.operation).toBe('+');
+    expect(calculator.previousOperand).toBe('12');
+
+    calculator.appendNumber(3);
+    calculator.appendNumber(4);
+    expect(calculator.currentOperand).toBe('34');
+    expect(calculator.operation).toBe('+');
+    expect(calculator.previousOperand).toBe('12');
+  })
   test('calculator clears', () => {
     const calculator = new calc.Calculator();
     calculator.clear();
